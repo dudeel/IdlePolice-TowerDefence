b517dc782c041752fd0eef5d20a504fab3911aa2
DuDeel <hopkalissa@gmail.com>
2023-08-05 20:19:14 +0400

    load card in UI

--- Assets/Scripts/Card/CardInfo.cs
+++ Assets/Scripts/Card/CardInfo.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CardInfo", menuName = "Card/CardInfo", order = 0)]
 public class CardInfo : ScriptableObject
 {
+    public int ID = 0;
     public Sprite Image = null;
 
     public string Name = "Null";
